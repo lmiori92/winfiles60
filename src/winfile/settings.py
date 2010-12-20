c957e589d56a012ec502371e8c2f9fec40e1f4cd
memoryS60 <memoryS60@gmail.com>
2010-12-20 19:20:09 +0000

    some ideas to split winfile into various modules

---
+++ src/winfile/settings.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+# GNU GENERAL PUBLIC LICENSE
+# Settings module for WinFile ( http://code.google.com/p/winfiles60/ ) - A powerful filemanager for the Symbian OS
+# Copyright   2008-2011 memoryN70 ( memoryS60@gmail.com )
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+Here to come:
+
+settings class
+color_init class
+translator class
+
+'''
